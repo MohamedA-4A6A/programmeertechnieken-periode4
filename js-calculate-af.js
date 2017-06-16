923e81b39a089901d1629b877e0319b83dc82991
MohamedA-4A6A <510178@edu.rocmn.nl>
2017-06-16 14:57:16 +0200

    alles gecommit

--- js-calculate-af.js
+++ js-calculate-af.js
@@ -1,19 +1,32 @@
 function calculate() {
 	var numbers = document.getElementById("numbers").value;
-	var operation = document.getElementById("operation").value;
+	
 	
 	var resultaat = 0;
 	
     // 34,5,6
 	var numbersArray = numbers.split(",");
+	if (document.getElementById("operation").value == "sum"){
 	
-	for(var index = 0; index < numbersArray.length; index++) {
+        for(var index = 0; index < numbersArray.length; index++) {
 		resultaat = parseInt(resultaat) + parseInt(numbersArray[index]);
-        
+    
 	}
-    console.log(resultaat);
-	
+        
+    
+    
 	document.getElementById("result").innerHTML = resultaat;
+    }
+    else if(document.getElementById("operation").value == "substract"){
+    resultaat = parseFloat(numbersArray[0]);
+    
+        for(var index = 1; index < numbersArray.length; index++) {
+		resultaat = resultaat - parseInt(numbersArray[index]);
+    
+	}
+   document.getElementById("result").innerHTML = resultaat;
 }
+}
+
 
 console.log("1");
